35c451dddd22a67abbeffdada59fd86f30bafddb
Humorloos <l.loos95@gmail.com>
2021-10-24 17:37:28 +0200

    Add bouldern to installed apps

--- pythonAnywhere/settings.py
+++ pythonAnywhere/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'pythonAnywhere.bouldern.apps.BouldernConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
