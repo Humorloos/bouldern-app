81bed6b6dc65a2d138479b34350a85c8be42ca60
Humorloos <l.loos95@gmail.com>
2021-10-24 17:20:13 +0200

    Add database model

--- pythonAnywhere/bouldern/models.py
+++ pythonAnywhere/bouldern/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Gym(models.Model):
+    name = models.CharField(max_length=100)
+    form_id = models.CharField(max_length=44)
+    sheet_id = models.CharField(max_length=44)
+    sheet_gid = models.CharField(max_length=10)
+    font_size = models.IntegerField()
+    gym_plan = models.FilePathField()
+
+
+class SectionBoundaries(models.Model):
+    x_from = models.FloatField()
+    x_to = models.FloatField()
+    y_from = models.FloatField()
+    y_to = models.FloatField()
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    gym = models.ForeignKey(Gym, on_delete=models.SET_NULL)
+    boundaries = models.ForeignKey(SectionBoundaries, on_delete=models.SET_NULL)
+
+
+class Wall(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+    section = models.ForeignKey(Section, on_delete=models.SET_NULL)
