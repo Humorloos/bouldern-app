e1545456df6f294b3989faf7ed2d2552f9129e4d
Humorloos <l.loos95@gmail.com>
2021-10-24 20:00:42 +0200

    Adjust apps script

--- pythonAnywhere/bouldern/apps.py
+++ pythonAnywhere/bouldern/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BouldernConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'bouldern'
+    verbose_name = 'Bouldern App'
