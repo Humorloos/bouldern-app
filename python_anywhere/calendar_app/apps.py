ba2377f20176b424ce4a25e50a331d79a96d534a
Humorloos <l.loos95@gmail.com>
2021-11-13 17:49:32 +0100

    Rename calendarApp to snake case

--- python_anywhere/calendar_app/apps.py
+++ python_anywhere/calendar_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CalendarappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calendarApp'
+    name = 'calendar_app'
--- python_anywhere/calendar_app/views.py
+++ python_anywhere/calendar_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from googleCalendarApp.calendar_handler import CalendarHandler
-from python_anywhere.calendarApp.constants import CHANNEL_ID_KEY
+from python_anywhere.calendar_app.constants import CHANNEL_ID_KEY
 
 
 @csrf_exempt
