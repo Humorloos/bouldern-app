2538b585bf34ab3b7e1d3122f56aac672c68dfd9
Humorloos <l.loos95@gmail.com>
2021-11-14 18:19:59 +0100

    Add Gym model

--- python_anywhere/bouldern/models.py
+++ python_anywhere/bouldern/models.py
@@ -1,8 +1,16 @@
 """Database models of bouldern app"""
 from django.contrib.gis.db.models import PointField
-from django.db import models
+from django.db.models import Model, CharField, ImageField
 
 
-class Boulder(models.Model):
+class Gym(Model):
+    name = CharField(max_length=128)
+    map = ImageField(max_length=256, upload_to='bouldern/gym_maps/')
+
+    def __str__(self):
+        return self.name
+
+
+class Boulder(Model):
     """A boulder in a gym"""
     coordinates = PointField()
