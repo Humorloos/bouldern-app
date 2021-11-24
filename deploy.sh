84d99acc79e5f4c3ddb360519bcac2b1ceed98a5
Humorloos <l.loos95@gmail.com>
2021-11-24 23:45:23 +0100

    Make deployment script build vue files correctly

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ git reset --hard origin/$1
 
 # compile vue files
 cd frontend
+source ~/nvm/nvm.sh
+nvm use node
+export NODE_OPTIONS=--openssl-legacy-provider
 npm ci
 npm run build
 cd ..
@@ -13,8 +16,6 @@ cd ..
 source ~/.env
 source ~/.virtualenvs/humorloos.pythonanywhere.com/bin/activate
 pip install -r requirements.txt --upgrade
-# dataclasses leads to error with pandas (see https://github.com/huggingface/transformers/issues/8638)
-pip uninstall dataclasses -y
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
