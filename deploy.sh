cc3b6e715f04de1e0c5af06aad8136f90875de68
Humorloos <l.loos95@gmail.com>
2021-11-24 21:51:40 +0100

    Uninstall dataclasses during deployment

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,8 @@ cd ..
 source ~/.env
 source ~/.virtualenvs/humorloos.pythonanywhere.com/bin/activate
 pip install -r requirements.txt --upgrade
+# dataclasses leads to error with pandas (see https://github.com/huggingface/transformers/issues/8638)
+pip uninstall dataclasses -y
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
