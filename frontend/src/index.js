57f2d81ac9ad5525d711c29cc1bb47ce92e6f599
Humorloos <l.loos95@gmail.com>
2021-12-02 23:38:47 +0100

    Remove vue test files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,6 @@
-import HelloWorld from "@/components/HelloWorld";
-import CounterModule from "@/vuex/vuex_module_counter";
-import {createAppInEl, createSharedStore} from "@/util/create_app_utils";
+import {createAppInEl} from "@/util/create_app_utils";
 import Modal from "@/components/Modal";
 import GymForm from "@/components/GymForm";
 
-// Initialize our shared store including the counter module
-let store = createSharedStore({
-    counter: CounterModule,
-});
-
 // Create and mount our apps
-createAppInEl(HelloWorld, store, "#hello_world_a", []);
-createAppInEl(HelloWorld, store, "#hello_world_b", []);
 createAppInEl({}, null, "#app", [GymForm, Modal]);
