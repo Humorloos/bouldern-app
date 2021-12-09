b42f43e19ec9de821b0bfe84c04535c373abb8f6
Lukas <l.loos95@gmail.com>
2021-12-09 19:51:11 +0100

    Shorten site usage

--- python_anywhere/bouldern/admin.py
+++ python_anywhere/bouldern/admin.py
@@ -1,10 +1,9 @@
 """This file is the home of model representations in the admin interface"""
-from django.contrib import admin
+from django.contrib.admin import site
 
 from python_anywhere.bouldern.models import Boulder, Gym, Color, DifficultyLevel
 
-# todo: check if this can be replaced by just site.register()
-admin.site.register(Color)
-admin.site.register(Boulder)
-admin.site.register(DifficultyLevel)
-admin.site.register(Gym)
+site.register(Color)
+site.register(Boulder)
+site.register(DifficultyLevel)
+site.register(Gym)
