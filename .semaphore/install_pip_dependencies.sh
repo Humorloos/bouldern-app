0f685b8995968b65315f05815a528f90266b4e66
Lukas <l.loos95@gmail.com>
2021-12-13 18:39:57 +0100

    Use pip instead of pip3 for installing GDAL python package

--- .semaphore/install_pip_dependencies.sh
+++ .semaphore/install_pip_dependencies.sh
@@ -4,5 +4,4 @@ sem-version python 3.9
 # ... so pip does the installation much faster.
 pip install -r requirements.txt --cache-dir .pip_cache
 # install gdal from local sources
-# todo: check if pip3 is really necessary and if we can cache this too
-pip3 install --global-option=build_ext --global-option="-I /usr/include/gdal" GDAL=="$(gdal-config --version)"
+pip install --global-option=build_ext --global-option="-I /usr/include/gdal" GDAL=="$(gdal-config --version)"
