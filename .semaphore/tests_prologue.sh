0b446120bca0e4116e785cd5c869c33321bb28b8
Lukas <l.loos95@gmail.com>
2021-12-14 00:26:32 +0100

    Cache spatialite

--- .semaphore/tests_prologue.sh
+++ .semaphore/tests_prologue.sh
@@ -1,10 +1,11 @@
 # restore private repositories
 cache restore googleCalendarApp
 cache restore GoogleApiHelper
+sudo -E cache restore spatialite
 checkout
 # install gdal
 . ./.semaphore/install_gdal.sh
 # install spatialite
-. ./.semaphore/install_spatialite.sh
+#. ./.semaphore/install_spatialite.sh
 # install pip dependencies
 . ./.semaphore/install_pip_dependencies.sh
