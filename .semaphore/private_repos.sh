7fed2c14df3fa02f4740a5632a3c322b672c0460
Lukas <l.loos95@gmail.com>
2021-12-13 17:46:39 +0100

    Only clone repos if not already cached

--- .semaphore/private_repos.sh
+++ .semaphore/private_repos.sh
@@ -1,12 +1,21 @@
 # Install private repository dependencies
-cd ..
-# Correct permissions since they are too open by default:
-chmod 0600 ~/.ssh/id_rsa_semaphoreci
-# Add the key to the ssh agent:
-ssh-add ~/.ssh/id_rsa_semaphoreci
-# clone repos
-git clone git@github.com:Humorloos/GoogleApiHelper.git
-cache store GoogleApiHelper ~/GoogleApiHelper
-git clone git@github.com:Humorloos/googleCalendarApp.git
-cache store googleCalendarApp ~/googleCalendarApp
-cd bouldern-app || exit
+GoogleApiHelper_cached=$(cache has_key GoogleApiHelper | grep -q "exists")
+googleCalendarApp_cached=$(cache has_key googleCalendarApp | grep -q "exists")
+
+if ! $GoogleApiHelper_cached || ! $googleCalendarApp_cached; then
+  # Correct permissions since they are too open by default:
+  chmod 0600 ~/.ssh/id_rsa_semaphoreci
+  # Add the key to the ssh agent:
+  ssh-add ~/.ssh/id_rsa_semaphoreci
+  cd ..
+  # clone repos if necessary
+  if ! $GoogleApiHelper_cached; then
+    git clone git@github.com:Humorloos/GoogleApiHelper.git
+    cache store GoogleApiHelper ~/GoogleApiHelper
+  fi
+  if ! $googleCalendarApp_cached; then
+    git clone git@github.com:Humorloos/googleCalendarApp.git
+    cache store googleCalendarApp ~/googleCalendarApp
+  fi
+  cd bouldern-app || exit
+fi
