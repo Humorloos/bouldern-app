a06776dcdd7e57e654a3e76f3a14c79d02786a08
Lukas <l.loos95@gmail.com>
2021-12-14 00:26:32 +0100

    Cache spatialite

--- .semaphore/install_spatialite.sh
+++ .semaphore/install_spatialite.sh
@@ -1,9 +1,16 @@
-# install spatialite
-cd ..
-wget https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0.tar.gz
-tar xaf libspatialite-4.3.0.tar.gz
-cd libspatialite-4.3.0 || exit
-./configure
-make
-sudo make install
-cd ../bouldern-app || exit
+
+if cache has_key googleCalendarApp | grep -q "exists"; then
+  echo "Installing spatialite"
+  cd ..
+  wget https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0.tar.gz
+  tar xaf libspatialite-4.3.0.tar.gz
+  cd libspatialite-4.3.0 || exit
+  ./configure
+  make
+  sudo make install
+  cd ../bouldern-app || exit
+  cache store spatialite /usr/local/lib/mod_spatialite.so
+else
+  echo "Spatialite already in cache, skipping installation"
+  sudo -E cache restore spatialite
+fi
