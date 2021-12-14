b3a50b5f1922a2497395ef5848b234dbc9d83bd0
Lukas <l.loos95@gmail.com>
2021-12-14 18:14:41 +0100

    Add UGC abstract model

--- python_anywhere/bouldern/models.py
+++ python_anywhere/bouldern/models.py
@@ -1,11 +1,21 @@
 """Database models of bouldern app"""
 from colorfield.fields import ColorField
 from django.contrib.gis.db.models import PointField
-from django.db.models import Model, CharField, ImageField, ForeignKey, SET_NULL, \
-    PositiveSmallIntegerField
+from django.db.models import Model, CharField, ImageField, ForeignKey, \
+    SET_NULL, PositiveSmallIntegerField, DateTimeField
+
+from python_anywhere.registration.models import User
 
 
-class Color(Model):
+class UGC(Model):
+    created_at = DateTimeField(auto_now_add=True)
+    created_by = ForeignKey(User, on_delete=SET_NULL, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Color(UGC):
     """Color of holds or difficulty levels"""
     name = CharField(max_length=128, unique=True)
     color = ColorField(default='#FF0000')
@@ -14,7 +24,7 @@ class Color(Model):
         return self.name
 
 
-class Gym(Model):
+class Gym(UGC):
     """A bouldering gym"""
     name = CharField(max_length=128, unique=True)
     map = ImageField(max_length=256, upload_to='bouldern/gym_maps/')
@@ -23,7 +33,7 @@ class Gym(Model):
         return self.name
 
 
-class DifficultyLevel(Model):
+class DifficultyLevel(UGC):
     """A difficulty level in a gym"""
     level = PositiveSmallIntegerField()
     color = ForeignKey(Color, on_delete=SET_NULL, null=True)
@@ -33,7 +43,10 @@ class DifficultyLevel(Model):
         return self.level
 
 
-class Boulder(Model):
+class Boulder(UGC):
     """A boulder in a gym"""
     coordinates = PointField()
     gym = ForeignKey(Gym, on_delete=SET_NULL, null=True)
+
+# todo: set created_by in reset_db
+# todo: find other places where content is created and set created_by there
