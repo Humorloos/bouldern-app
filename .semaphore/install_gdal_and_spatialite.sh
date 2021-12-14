6941766524dc6864f5f0dc15a33af379cad16506
Lukas <l.loos95@gmail.com>
2021-12-14 01:02:42 +0100

    Cache whole /usr directory

---
+++ .semaphore/install_gdal_and_spatialite.sh
@@ -0,0 +1,25 @@
+# Install gdal and spatialite
+if ! cache has_key usr_directory | grep -q "exists"; then
+  echo "installing gdal"
+  # install GDAL from ubuntugis/ppa
+  # echo -ne '\n' | for automatically accepting with [ENTER]
+  echo -ne '\n' | sudo add-apt-repository ppa:ubuntugis/ppa
+  sudo apt-get update
+  # -y for automatically answering with y
+  sudo apt-get install -y libgdal-dev gdal-bin
+  export CPLUS_INCLUDE_PATH=/usr/include/gdal
+  export C_INCLUDE_PATH=/usr/include/gdal
+  echo "installing spatialite"
+  # install spatialite
+  cd ..
+  wget https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0.tar.gz
+  tar xaf libspatialite-4.3.0.tar.gz
+  cd libspatialite-4.3.0 || exit
+  ./configure
+  make
+  sudo make install
+  cd ../bouldern-app || exit
+  echo "caching /usr"
+else
+  echo "/usr already in cache, skipping installation"
+fi
--- .semaphore/tests_prologue.sh
+++ .semaphore/tests_prologue.sh
@@ -1,10 +1,12 @@
 # restore private repositories
 cache restore googleCalendarApp
 cache restore GoogleApiHelper
+sudo -E cache restore usr_directory
+
 checkout
-# install gdal
-. ./.semaphore/install_gdal.sh
-# install spatialite
-. ./.semaphore/install_spatialite.sh
+## install gdal
+#. ./.semaphore/install_gdal.sh
+## install spatialite
+#. ./.semaphore/install_spatialite.sh
 # install pip dependencies
 . ./.semaphore/install_pip_dependencies.sh
