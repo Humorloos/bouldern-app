e164a270f4c673ab5d287c8da525425b2e789e7a
Lukas <l.loos95@gmail.com>
2021-12-14 16:53:50 +0100

    Add e2e tests to pipeline

--- .semaphore/backend_prologue.sh
+++ .semaphore/backend_prologue.sh
@@ -1,10 +1,17 @@
+#before checkout
+
 # restore private repositories
+cd ~ || exit
 cache restore googleCalendarApp
 cache restore GoogleApiHelper
 # restore gdal
 cache restore gdal_files
 sudo cp -r /home/semaphore/gdal_files/usr /
 
-checkout
+cd ~/bouldern-app || exit
+
 # install pip dependencies
 . ./.semaphore/install_pip_dependencies.sh
+
+python manage.py makemigrations
+python manage.py migrate
