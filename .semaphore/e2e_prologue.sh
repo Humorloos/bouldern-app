d75aef9439dc575c8e356a09f3cf30bc41ed0103
Lukas <l.loos95@gmail.com>
2021-12-15 17:42:10 +0100

    Cache .npm instead of node_modules

--- .semaphore/e2e_prologue.sh
+++ .semaphore/e2e_prologue.sh
@@ -1,3 +1,11 @@
 . .semaphore/frontend_prologue.sh
 cd ..
+cd ~/bouldern-app/frontend || exit
+# todo: check if moving npm ci here breaks pipeline,
+# last time it did not work in frontend_prologue
+# last time it worked just at the beginning of backend_prologue.sh
+# restore npm
+cache restore "$NPM_KEY"
+npm ci
+cd ..
 . .semaphore/backend_prologue.sh
