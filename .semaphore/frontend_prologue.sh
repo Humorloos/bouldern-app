ea8a5fa347f213bdda577d7c9267e1aeac4cc920
Lukas <l.loos95@gmail.com>
2021-12-15 09:54:21 +0100

    Update npm in e2e prologue

--- .semaphore/frontend_prologue.sh
+++ .semaphore/frontend_prologue.sh
@@ -8,3 +8,6 @@ npm --version
 cd frontend || exit
 # restore node_modules
 cache restore node_modules
+# see https://community.atlassian.com/t5/Bitbucket-questions/Module-not-found-Error-Can-t-resolve-cypress-file-upload-in-opt/qaq-p/1184319
+npm update
+npm install
