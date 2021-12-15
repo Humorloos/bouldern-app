fe5643a9618d29076ac5f53368a3b44450d99d34
Lukas <l.loos95@gmail.com>
2021-12-15 12:11:31 +0100

    Cache .npm instead of node_modules

--- .semaphore/e2e_prologue.sh
+++ .semaphore/e2e_prologue.sh
@@ -1,3 +1,5 @@
 . .semaphore/frontend_prologue.sh
+npm ci
 cd ..
 . .semaphore/backend_prologue.sh
+# todo: check if moving npm ci here (beginning of jobs) breaks pipeline, if not, move into end of e2e_prologue and further until pipeline breaks
