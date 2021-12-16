2bd21d90ffa1cf2ac1ad2e97d38c8ecfe0e5a112
Lukas <l.loos95@gmail.com>
2021-12-16 14:57:52 +0100

    Cache spatialite files

--- .semaphore/install_gdal_and_spatialite.sh
+++ .semaphore/install_gdal_and_spatialite.sh
@@ -1,7 +1,7 @@
 # Install gdal and spatialite
 if ! cache has_key gdal_files | grep -q "exists"; then
-  echo "installing gdal"
   # install GDAL from ubuntugis/ppa
+  echo "installing gdal"
   # echo -ne '\n' | for automatically accepting with [ENTER]
   echo -ne '\n' | sudo add-apt-repository ppa:ubuntugis/ppa
   sudo apt-get update
@@ -10,6 +10,18 @@ if ! cache has_key gdal_files | grep -q "exists"; then
   # todo: check if this is necessary
   export CPLUS_INCLUDE_PATH=/usr/include/gdal
   export C_INCLUDE_PATH=/usr/include/gdal
+
+  # install spatialite
+  echo "installing spatialite"
+  cd ..
+  wget https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0.tar.gz
+  tar xaf libspatialite-4.3.0.tar.gz
+  cd libspatialite-4.3.0 || exit
+  ./configure
+  make
+  sudo make install
+  cd ../bouldern-app || exit
+
   # copy all files in gdal_files to gdal_paths folder for caching
   while read -r p; do
     mkdir --parents "$(dirname ~/gdal_files"$p")"
@@ -17,22 +29,6 @@ if ! cache has_key gdal_files | grep -q "exists"; then
   done < /home/semaphore/bouldern-app/.semaphore/gdal_paths
   # cache collected files
   cache store gdal_files /home/semaphore/gdal_files
-#todo:
-  # install spatialite
-#  todo: check if spatialite is not already installed by previous command (Get:81 http://ppa.launchpad.net/ubuntugis/ppa/ubuntu bionic/main amd64 libspatialite-dev amd64 4.3.0a-5build1+bionic1 [1,370 kB])
-# todo: check if setting spatialite library path in semaphore env to Only in /usr/lib/x86_64-linux-gnu/libspatialite.so.7 works (see https://humorloos.semaphoreci.com/jobs/c2d20510-66d5-4bbe-a8ab-9d0804f95b47 which says in error that this file may work)
-#  echo "installing spatialite"
-#  cd ..
-#  wget https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0.tar.gz
-#  tar xaf libspatialite-4.3.0.tar.gz
-#  cd libspatialite-4.3.0 || exit
-#  ./configure
-#  make
-#  sudo make install
-#  cd ../bouldern-app || exit
-#  echo "caching /usr"
-#  cache delete usr_directory
-#  cache store usr_directory /usr
 else
   echo "gdal_files already in cache, skipping installation"
 fi
