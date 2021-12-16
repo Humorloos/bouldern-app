b4c1adb8346ebe47914e052408f2457096284f13
Lukas <l.loos95@gmail.com>
2021-12-16 23:51:37 +0100

    Add documentation

--- python_anywhere/bouldern/providers.py
+++ python_anywhere/bouldern/providers.py
@@ -1,7 +1,13 @@
+"""Custom providers for faker"""
 from django.contrib.gis.geos import Point
 from faker.providers.geo import Provider
 
 
 class GeoProvider(Provider):
+    """Provider of geographic data"""
     def point(self):
+        """
+        Provides a point in geojson format with random latitude and longitude
+        :return: the point in geojson format
+        """
         return Point(self.latlng()).geojson
