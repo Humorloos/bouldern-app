4c373af2fd157dc0d270066364af6061ed3875ab
Lukas <l.loos95@gmail.com>
2021-12-16 13:22:58 +0100

    Cache gdal files

--- .semaphore/install_gdal_and_spatialite.sh
+++ .semaphore/install_gdal_and_spatialite.sh
@@ -9,10 +9,21 @@
   sudo apt-get install -y libgdal-dev=2.4.2+dfsg-1~bionic0 gdal-bin=2.4.2+dfsg-1~bionic0
   export CPLUS_INCLUDE_PATH=/usr/include/gdal
   export C_INCLUDE_PATH=/usr/include/gdal
-  echo "installing spatialite"
+  while read -r p; do
+    mkdir --parents "$(dirname ~/gdal_files"$p")"
+    cp -r "$p" ~/gdal_files"$p"
+  done < /home/semaphore/bouldern-app/.semaphore/gdal_paths
+  cache delete gdal_files # todo: remove this
+  cache store gdal_files /home/semaphore/gdal_files
+#  todo: write script that caches each line in gdal_paths,
+#   saves list of cache keys
+#   and caches this list with key gdal_installation
+#todo verify that caching worked by running Pylint task
+#todo:
   # install spatialite
 #  todo: check if spatialite is not already installed by previous command (Get:81 http://ppa.launchpad.net/ubuntugis/ppa/ubuntu bionic/main amd64 libspatialite-dev amd64 4.3.0a-5build1+bionic1 [1,370 kB])
 # todo: check if setting spatialite library path in semaphore env to Only in /usr/lib/x86_64-linux-gnu: libspatialite.so.7 works (see https://humorloos.semaphoreci.com/jobs/c2d20510-66d5-4bbe-a8ab-9d0804f95b47 which says in error that this file may work)
+#  echo "installing spatialite"
 #  cd ..
 #  wget https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0.tar.gz
 #  tar xaf libspatialite-4.3.0.tar.gz
