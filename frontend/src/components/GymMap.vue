44081ef62b8b5c404791c9a4597f9a6c756e4df9
Lukas <l.loos95@gmail.com>
2021-12-17 19:07:24 +0100

    Add first dummy for gym map component

---
+++ frontend/src/components/GymMap.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>this is the gym map for gym {{ gymName }}</span>
+</template>
+
+<script>
+export default {
+  name: 'GymMap',
+  props: {
+    gymName: {
+      type: String,
+      default: 'wrong',
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
