9a7985bb8eee5dae8515575f94d918457816148b
Lukas <l.loos95@gmail.com>
2021-12-21 16:27:10 +0100

    Display basic skeleton at index

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template />
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,6 +10,20 @@ const pages = {
     entry: './src/gym_map.js',
     chunks: ['chunk-vendors'],
   },
+  'index': {
+    // entry for the page
+    entry: 'src/main.js',
+    // the source template
+    template: 'public/index.html',
+    // output as dist/index.html
+    filename: 'index.html',
+    // when using title option, template title tag needs to be
+    // <title><%= htmlWebpackPlugin.options.title %></title>
+    title: 'Index Page',
+    // chunks to include on this page, by default includes
+    // extracted common chunks and vendor chunks.
+    chunks: ['chunk-vendors', 'chunk-common', 'index'],
+  },
 };
 
 const path = require('path');
@@ -17,12 +31,14 @@ const path = require('path');
 module.exports = {
   pages: pages,
   filenameHashing: false, // Django will hash file names, not webpack
+  // Setting this to false can speed up production builds if you don't need
+  // source maps for production.
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production' ?
-      // signals to django-webpack-loader to fall back to Django's standard
-      // static finder behavior
-      '' :
-      'https://localhost:8080/', // in development, use own webpack development server
+        // signals to django-webpack-loader to fall back to Django's standard
+        // static finder behavior
+        '' :
+        'https://localhost:8080/', // in development, use own webpack development server
   outputDir: path.resolve(__dirname, '../python_anywhere/static/vue/'),
   chainWebpack: (config) => {
     // extract vendor javascripts into single shared bundle to allow browsers to
@@ -40,7 +56,7 @@ module.exports = {
         });
     // delete plugins that create html files for bundles since we serve them
     // from django templates
-    Object.keys(pages).forEach((page) => {
+    ['gym_form', 'gym_map'].forEach((page) => {
       config.plugins.delete(`html-${page}`);
       config.plugins.delete(`preload-${page}`);
       config.plugins.delete(`prefetch-${page}`);
