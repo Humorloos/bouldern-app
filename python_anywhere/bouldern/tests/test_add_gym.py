7149bbb59b048215eaf85f71c5dcb67be5bba592
Lukas <l.loos95@gmail.com>
2021-12-28 18:41:14 +0100

    temp

--- python_anywhere/bouldern/tests/test_add_gym.py
+++ python_anywhere/bouldern/tests/test_add_gym.py
@@ -75,3 +75,19 @@ def test_add_gym_rest(logged_in_client):
     # Then
     gym = Gym.objects.first()
     assert_correct_gym(gym, payload, user)
+
+
+def test_add_gym_rest(logged_in_client):
+    """Test that post method works correctly"""
+    # Given
+    from python_anywhere.bouldern.factories import GymFactory
+    payload = {key: GymFactory.stub().__dict__[key]
+               for key in ['map', 'name']}
+    client, user = logged_in_client
+
+    # When
+    client.post(reverse(AddGymRest.name), data=payload, format='multipart')
+
+    # Then
+    gym = Gym.objects.first()
+    assert_correct_gym(gym, payload, user)
