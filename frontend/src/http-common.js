490e5764b5f2d1cf7ea36c74e36e00de88150d3f
Lukas <l.loos95@gmail.com>
2021-12-29 15:06:21 +0100

    147_adjust_axios_url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.humorloos.pythonanywhere.com:8000',
+  baseURL: process.env.NODE_ENV === 'production' ?
+        // signals to django-webpack-loader to fall back to Django's standard
+        // static finder behavior
+        'https://humorloos.pythonanywhere.com/' :
+        'https://127.0.0.1:8080/', // in development, use own webpack development server,
   headers: {
     'Content-type': 'application/json',
   },
