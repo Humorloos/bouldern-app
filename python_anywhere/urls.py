96ac6600aeb962f15ef806a89807bf5382439057
Lukas <l.loos95@gmail.com>
2021-12-30 10:35:12 +0100

    Only fall back in production

--- python_anywhere/urls.py
+++ python_anywhere/urls.py
@@ -44,6 +44,5 @@ if DEVELOPMENT:
     urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
 else:
     urlpatterns += [path('', index, name='index')]
-
-# History API Fallback
-urlpatterns += [re_path(r'^.*', index, name='index')]
+    # History API Fallback
+    urlpatterns += [re_path(r'^.*', index, name='index')]
