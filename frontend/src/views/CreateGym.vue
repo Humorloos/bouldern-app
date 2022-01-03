d88c21d60f4ca495d03b5921be2c38682d3d401d
Lukas <l.loos95@gmail.com>
2022-01-03 12:12:45 +0100

    Add difficultyLevelSelect to CreateGym.vue

--- frontend/src/views/CreateGym.vue
+++ frontend/src/views/CreateGym.vue
@@ -21,17 +21,23 @@
         @change="onFileChange"
       >
     </div>
+
+    <difficulty-level-select
+      :options="options"
+    />
   </vue-form>
 </template>
 
 <script>
 import VueForm from '@/components/VueForm';
 import {mapState} from 'vuex';
+import DifficultyLevelSelect from '@/components/DifficultyLevelSelect';
 
 export default {
   name: 'CreateGym',
   components: {
     VueForm,
+    DifficultyLevelSelect,
   },
   data() {
     return {
@@ -40,12 +46,19 @@ export default {
       },
       map: undefined,
       apiPath: '/bouldern/rest/add-gym/',
+      options: [{
+        'label': 'default',
+        'style': {'color': '#FF7514'},
+      }],
     };
   },
   computed: {
     ...mapState([
       'authToken',
     ]),
+  },
+  mounted() {
+    require('vue-select/dist/vue-select.css');
   },
   methods: {
     onFileChange(e) {
@@ -55,10 +68,12 @@ export default {
       const formData = new FormData();
       formData.append('map', this.map);
       this.axios.patch(`${this.apiPath}${response.data.id}/`, formData,
-          {headers: {
-            'authorization': `Bearer ${this.authToken.token}`,
-            'content-type': 'multipart/form-data',
-          }});
+          {
+            headers: {
+              'authorization': `Bearer ${this.authToken.token}`,
+              'content-type': 'multipart/form-data',
+            },
+          });
     },
   },
 };
