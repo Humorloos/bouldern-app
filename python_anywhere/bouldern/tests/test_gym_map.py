2cc225ec30aa56d330245923531d78daf90be402
Lukas <l.loos95@gmail.com>
2022-01-04 10:05:52 +0100

    Add tests for Boulder API

--- python_anywhere/bouldern/tests/test_gym_map.py
+++ python_anywhere/bouldern/tests/test_gym_map.py
@@ -2,10 +2,11 @@
 import pytest
 from django.urls import reverse
 from faker import Faker
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
 
-from python_anywhere.bouldern.models import Boulder
-from python_anywhere.bouldern.views import gym_map
 from python_anywhere.accounts.factories import UserFactory
+from python_anywhere.bouldern.models import Boulder
+from python_anywhere.bouldern.views import gym_map, BoulderAPI
 
 
 @pytest.mark.django_db
@@ -42,3 +43,43 @@ def test_gym_map(client):
         assert boulder.coordinates.geojson in boulders_payload.values()
         assert boulder.gym == gym
     assert response.url == reverse(gym_map, kwargs={'gym_name': gym.name})
+
+
+def test_boulder_api_post(logged_in_client_rest):
+    """Test that post method works correctly"""
+    # Given
+    client, user = logged_in_client_rest
+
+    from python_anywhere.bouldern.factories import GymFactory
+    gym = GymFactory()
+
+    from python_anywhere.bouldern.factories import BoulderFactory
+    payload = {f'coordinates': BoulderFactory.stub(
+        gym=gym).coordinates.geojson, 'gym': gym.pk}
+    # When
+    response = client.post(BoulderAPI().reverse_action('list', args=[gym.pk]),
+                           data=payload, format='json')
+    # Then
+    assert response.status_code == HTTP_201_CREATED
+    boulder = Boulder.objects.first()
+    assert boulder.created_by == user
+    assert boulder.coordinates.geojson == payload['coordinates']
+    assert boulder.gym == gym
+
+
+def test_boulder_api_get(logged_in_client_rest):
+    client, user = logged_in_client_rest
+    from python_anywhere.bouldern.factories import GymFactory
+    correct_gym = GymFactory()
+    incorrect_gym = GymFactory()
+    from python_anywhere.bouldern.factories import BoulderFactory
+    correct_boulders = {BoulderFactory(gym=correct_gym) for _ in range(3)}
+    incorrect_boulders = {BoulderFactory(gym=incorrect_gym) for _ in range(3)}
+    # when
+    response = client.get(
+        BoulderAPI().reverse_action('list', args=[correct_gym.pk]))
+    # then
+    assert response.status_code == HTTP_200_OK
+    boulders = set(response.data.serializer.instance)
+    assert boulders.intersection(correct_boulders) == boulders
+    assert boulders.intersection(incorrect_boulders) == set()
