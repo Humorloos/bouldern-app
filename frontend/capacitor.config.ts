f50dd054006aa1d8cf715fe8e5c7d19ebe464bf6
Lukas <l.loos95@gmail.com>
2022-01-06 22:12:58 +0100

    Add ionic android project

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'humorloos.pythonanywhere.com',
+  appName: 'Bouldern App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
