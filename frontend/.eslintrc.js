010ee6beeeb6bd6e354d6ecd4a45275a8c57e183
Lukas <l.loos95@gmail.com>
2022-01-10 10:21:07 +0100

    Restructure eslintrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -12,17 +12,7 @@ module.exports = {
   plugins: ['jsdoc'],
   rules: {
     'valid-jsdoc': 0, // eslint-jsdoc takes care of jsdoc validation
-    'jsdoc/require-param-type': 0, // for now, no types in jsdoc
-    'jsdoc/require-param': [2, {
-      contexts: [
-        // no params for store methods for now
-        'ArrowFunctionExpression:not(CallExpression[callee.name ="createStore"] ArrowFunctionExpression)',
-        'FunctionDeclaration',
-        'FunctionExpression:not(CallExpression[callee.name ="createStore"] FunctionExpression)'
-      ],
-    }],
     'jsdoc/require-jsdoc': [2, {
-      'publicOnly': false,
       require: {
         'MethodDefinition': true,
         'FunctionExpression': true,
@@ -31,5 +21,15 @@ module.exports = {
         'ArrowFunctionExpression': true,
       },
     }],
+    'jsdoc/require-description': 2,
+    'jsdoc/require-param': [2, {
+      contexts: [
+        // no params for store methods for now
+        'ArrowFunctionExpression:not(CallExpression[callee.name ="createStore"] ArrowFunctionExpression)',
+        'FunctionDeclaration',
+        'FunctionExpression:not(CallExpression[callee.name ="createStore"] FunctionExpression)'
+      ],
+    }],
+    'jsdoc/require-param-type': 0, // for now, no types in jsdoc
   },
 };
