66394b42d78c0fe6022785ed06c154fddd59bb39
Lukas <l.loos95@gmail.com>
2022-01-21 23:03:43 +0100

    feat(views): use UpdateModelMixin in UserAPI

--- python_anywhere/accounts/views.py
+++ python_anywhere/accounts/views.py
@@ -1,14 +1,14 @@
 """This file contains views for the accounts app"""
 
 # accounts/views.py
-from rest_framework.mixins import DestroyModelMixin
+from rest_framework.mixins import UpdateModelMixin
 
 from python_anywhere.accounts.models import User
 from python_anywhere.accounts.serializers import UserSerializer
 from python_anywhere.views import ReversibleViewSet
 
 
-class UserAPI(ReversibleViewSet, DestroyModelMixin):
+class UserAPI(ReversibleViewSet, UpdateModelMixin):
     """Rest API for reading and creating boulders in a specific gym"""
     basename = 'user'
     queryset = User.objects.all()
