43b805bd14d044691d5abfcdd255ecb88acfec73
Lukas <l.loos95@gmail.com>
2022-01-22 11:36:53 +0100

    test(factories): instantiate email address when using UserFactory

--- python_anywhere/accounts/factories.py
+++ python_anywhere/accounts/factories.py
@@ -3,10 +3,22 @@ This script contains factories for building model instances of the accounts
 app
 """
 
-from factory import PostGenerationMethodCall, Faker
+from allauth.account.models import EmailAddress
+from factory import PostGenerationMethodCall, Faker, RelatedFactory, \
+    SelfAttribute
 from factory.django import DjangoModelFactory
 
 from python_anywhere.accounts.models import User
+
+
+class EmailAddressFactory(DjangoModelFactory):
+    """Factory for building email address instances"""
+    
+    class Meta:
+        model = EmailAddress
+
+    verified = 1
+    primary = 1
 
 
 class UserFactory(DjangoModelFactory):
@@ -21,3 +33,9 @@ class UserFactory(DjangoModelFactory):
 
     is_superuser = False
     is_staff = False
+
+    related_email = RelatedFactory(
+        EmailAddressFactory,
+        factory_related_name='user',
+        email=SelfAttribute('..email'),
+    )
