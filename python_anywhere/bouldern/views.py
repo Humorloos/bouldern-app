f94dadaab1ae591f33fb730ee4ddce8604d3114e
Lukas <l.loos95@gmail.com>
2022-01-29 19:51:13 +0100

    refactor(views): extract CreateUGCMixin

--- python_anywhere/bouldern/views.py
+++ python_anywhere/bouldern/views.py
@@ -1,7 +1,7 @@
 """Module containing the views of the bouldern app"""
 from django.shortcuts import render
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from url_filter.integrations.drf import DjangoFilterBackend
 
 from python_anywhere.bouldern.models import Boulder, Gym, Color
@@ -10,18 +10,23 @@ from python_anywhere.bouldern.serializers import GymSerializer, ColorSerializer,
 from python_anywhere.views import ReversibleViewSet
 
 
-class ColorAPI(ReversibleViewSet, ListModelMixin, CreateModelMixin):
+class CreateUGCMixin(CreateModelMixin, GenericViewSet):
+    """Mixin for creating UGC instances"""
+
+    def perform_create(self, serializer, **kwargs):
+        serializer.save(created_by=self.request.user,
+                        modified_by=self.request.user,
+                        **kwargs)
+
+
+class ColorAPI(ReversibleViewSet, ListModelMixin, CreateUGCMixin):
     """REST API for adding colors"""
     basename = 'color'
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user,
-                        modified_by=self.request.user)
 
-
-class GymAPI(ReversibleViewSet, ModelViewSet):
+class GymAPI(ReversibleViewSet, ModelViewSet, CreateUGCMixin):
     """Rest API for adding gyms"""
     basename = 'gym'
     queryset = Gym.objects.all()
@@ -29,12 +34,8 @@ class GymAPI(ReversibleViewSet, ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filter_fields = ['name']
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user,
-                        modified_by=self.request.user)
 
-
-class BoulderAPI(ReversibleViewSet, ModelViewSet):
+class BoulderAPI(ReversibleViewSet, ModelViewSet, CreateUGCMixin):
     """Rest API for reading and creating boulders in a specific gym"""
     basename = 'boulder'
     queryset = Boulder.objects.all()
@@ -45,10 +46,9 @@ class BoulderAPI(ReversibleViewSet, ModelViewSet):
     def get_queryset(self):
         return Boulder.objects.filter(gym_id=self.kwargs['gym_pk'])
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user,
-                        modified_by=self.request.user,
-                        gym=Gym.objects.get(pk=self.kwargs['gym_pk']))
+    def perform_create(self, serializer, **kwargs):
+        super().perform_create(
+            serializer, gym=Gym.objects.get(pk=self.kwargs['gym_pk']))
 
 
 def index(request):
