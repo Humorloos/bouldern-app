b0db1ba1ab964e31e66c0150a628907823fe21b1
Lukas <l.loos95@gmail.com>
2022-02-09 18:02:09 +0100

    feat(vue): show HomeView at index

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <gym-map
+    v-if="activeGym !== null"
+  />
+  <app-view
+    v-else
+    :gym-search="false"
+  >
+    <template #main>
+      <v-list>
+        <gym-search />
+      </v-list>
+    </template>
+  </app-view>
+</template>
+<script>
+/** @file the view shown at the index */
+import GymMap from './GymMap.vue';
+import {useStore} from 'vuex';
+import GymSearch from '../components/GymSearch.vue';
+import AppView from '../components/AppView.vue';
+
+export default {
+  name: 'HomeView',
+  components: {AppView, GymSearch, GymMap},
+  setup() {
+    const store = useStore();
+    return {
+      activeGym: store.state.activeGym,
+    };
+  },
+};
+</script>
+
+<style scoped/>
