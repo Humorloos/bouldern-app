0b431f3bd59f4e77a6902ea44105e175bee7b74f
Lukas <l.loos95@gmail.com>
2022-02-13 18:03:41 +0100

    feat(urls): add password reset confirm url

--- python_anywhere/accounts/urls.py
+++ python_anywhere/accounts/urls.py
@@ -1,10 +1,16 @@
 """URL mappings for accounts app"""
 
 # accounts/urls.py
+from django.urls import re_path
 from rest_framework.routers import SimpleRouter
 
 from python_anywhere.accounts.views import UserAPI
+from python_anywhere.bouldern.views import index
 
 router = SimpleRouter()
 router.register(UserAPI.basename, UserAPI)
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    re_path(r'^password/reset/change/(?P<uidb64>[0-9A-Za-z_\-]+)/'
+            r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
+            index, name='password_reset_confirm')
+]
