e58bf71cb39a23bb72c14261ba16a9b1aa0b7f2d
Lukas <l.loos95@gmail.com>
2022-02-13 18:41:25 +0100

    test(testResetPassword): add test for reset password confirmation

--- python_anywhere/accounts/tests/test_reset_password.py
+++ python_anywhere/accounts/tests/test_reset_password.py
@@ -1,4 +1,5 @@
 from django.core import mail
+from faker import Faker
 from rest_framework.reverse import reverse
 from rest_framework.status import HTTP_200_OK
 from rest_framework.test import APIClient
@@ -19,3 +20,24 @@ def test_reset_password(db):
     # Then
     assert response.status_code == HTTP_200_OK
     assert 'password reset' in mail.outbox[0].body
+
+    # Given
+    reset_url_components = response.context[0].get('password_reset_url')\
+        .split('/')
+    token = reset_url_components[-2]
+    uid = reset_url_components[-3]
+    password = Faker().password()
+    confirm_payload = {
+        'token': token,
+        'uid': uid,
+        'new_password1': password,
+        'new_password2': password
+    }
+
+    # When
+    response = client.post(reverse('rest_password_reset_confirm'),
+                           data=confirm_payload, format='json')
+
+    # Then
+    assert response.status_code == HTTP_200_OK
+    assert client.login(username=user.email, password=password)
