25e34d7cdbad251187532f37bd3be7a008edf934
Lukas <l.loos95@gmail.com>
2022-02-14 18:55:51 +0100

    test(gymFavorites): test gym favorites api get method

--- python_anywhere/bouldern/tests/test_favorite_gym.py
+++ python_anywhere/bouldern/tests/test_favorite_gym.py
@@ -1,5 +1,7 @@
-from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, \
+    HTTP_200_OK
 
+from python_anywhere.accounts.factories import UserFactory
 from python_anywhere.bouldern.models import FavoriteGym
 from python_anywhere.bouldern.views import FavoriteGymAPI
 
@@ -41,3 +43,25 @@ def test_remove_favorite_gym(logged_in_client_rest, colors):
     assert response.status_code == HTTP_204_NO_CONTENT
     deleted_favorite = FavoriteGym.objects.get(pk=favorite_gym.pk)
     assert not deleted_favorite.is_active
+
+
+def test_get_favorite_gyms(logged_in_client_rest, colors):
+    # given
+    from python_anywhere.bouldern.factories import FavoriteGymFactory
+    favorite_gyms = FavoriteGymFactory.create_batch(5)
+    old_favorites = FavoriteGymFactory.create_batch(2, is_active=False)
+    other_user = UserFactory()
+    favorites_by_other_user = FavoriteGymFactory.create_batch(
+        2, created_by=other_user)
+
+    client, user = logged_in_client_rest
+
+    # when
+    response = client.get(FavoriteGymAPI().reverse_action('list'))
+
+    # then
+    assert response.status_code == HTTP_200_OK
+    returned_favorites = response.data.serializer.instance
+    assert all(g in returned_favorites for g in favorite_gyms)
+    assert not any(g in returned_favorites for g in old_favorites)
+    assert not any(g in returned_favorites for g in favorites_by_other_user)
