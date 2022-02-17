db2aa78e29026b7c8a5c25e8ef8f3fe4fda98c7c
Lukas <l.loos95@gmail.com>
2022-02-17 17:41:36 +0100

    ci(pipeline): install gdal directly in docker image

--- .semaphore/install_pip_dependencies.sh
+++ .semaphore/install_pip_dependencies.sh
@@ -2,5 +2,3 @@
 cache restore
 # ... so pip does the installation much faster.
 pip install -r requirements.txt --cache-dir .pip_cache
-# install gdal from local sources
-pip install --global-option=build_ext --global-option="-I /usr/include/gdal" GDAL=="$(gdal-config --version)"
