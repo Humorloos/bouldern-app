876c827ec575a6fb5bddb2b39bcac59b6f492037
Lukas <l.loos95@gmail.com>
2022-02-19 19:43:11 +0100

    feat(views): allow destroying users via delete

--- python_anywhere/accounts/views.py
+++ python_anywhere/accounts/views.py
@@ -1,15 +1,19 @@
 """This file contains views for the accounts app"""
 
 # accounts/views.py
-from rest_framework.mixins import UpdateModelMixin
+from rest_framework.mixins import DestroyModelMixin
 
 from python_anywhere.accounts.models import User
 from python_anywhere.accounts.serializers import UserSerializer
 from python_anywhere.views import ReversibleViewSet
 
 
-class UserAPI(ReversibleViewSet, UpdateModelMixin):
+class UserAPI(ReversibleViewSet, DestroyModelMixin):
     """Rest API for deleting users"""
     basename = 'user'
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
