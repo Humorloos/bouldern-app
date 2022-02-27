6ed2cd33bfe5b983420a5453554f9bafc65dd758
Lukas <l.loos95@gmail.com>
2022-02-27 14:01:42 +0100

    temp

--- .semaphore/deploy_python_anywhere.sh
+++ .semaphore/deploy_python_anywhere.sh
@@ -1,25 +1,12 @@
-# Install private repository dependencies
-GoogleApiHelper_cached=$(cache has_key GoogleApiHelper)
-googleCalendarApp_cached=$(cache has_key googleCalendarApp)
-
-if [[ $GoogleApiHelper_cached == *"exists"* ]] && [[ $googleCalendarApp_cached == *"exists"* ]]; then
-  echo "Private repositories already in cache, skipping installation"
+if [[ $(git log -1 --pretty=%B) == "chore(release): "* ]]; then
+  echo "Skipping deployment to pythonanywhere due to release commit"
 else
-  # Correct permissions since they are too open by default:
-  chmod 0600 ~/.ssh/id_rsa_semaphoreci
-  # Add the key to the ssh agent:
-  ssh-add ~/.ssh/id_rsa_semaphoreci
-  cd ..
-  # clone repos if necessary
-  if [[ $GoogleApiHelper_cached == *"doesn't"* ]]; then
-    echo "Installing and caching private repository GoogleApiHelper"
-    git clone git@github.com:Humorloos/GoogleApiHelper.git
-    cache store GoogleApiHelper ~/GoogleApiHelper
-  fi
-  if [[ $googleCalendarApp_cached == *"doesn't"* ]]; then
-    echo "Installing and caching private repository googleCalendarApp"
-    git clone git@github.com:Humorloos/googleCalendarApp.git
-    cache store googleCalendarApp ~/googleCalendarApp
-  fi
-  cd "$PROJECT_DIR" || exit
+  checkout
+  chmod 0600 ~/.ssh/id_rsa_pa
+  ssh-keyscan -H ssh.pythonanywhere.com >> ~/.ssh/known_hosts
+  ssh-add ~/.ssh/id_rsa_pa
+  cd ~ || exit
+  sftp "${SSH_USER}"@ssh.pythonanywhere.com <<< $'put bouldern-app/deploy.sh deploy.sh\n put .env_production .env'
+  # shellcheck disable=SC2029
+  ssh "${SSH_USER}"@ssh.pythonanywhere.com bash deploy.sh "${SEMAPHORE_GIT_PR_BRANCH-master}"
 fi
