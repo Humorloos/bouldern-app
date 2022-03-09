f9ca3caebcbd57d4b9dc26d1cfb34e3c4a9cafc7
Lukas <l.loos95@gmail.com>
2022-03-09 17:17:29 +0100

    feat(ChangePassword): validate passwords

--- frontend/src/views/ChangePassword.vue
+++ frontend/src/views/ChangePassword.vue
@@ -2,39 +2,52 @@
   <app-view>
     <template #main>
       <v-container>
-        <v-row>
-          <v-col>Change password</v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-text-field
-              id="id_password1"
-              v-model="password1"
-              label="Password"
-              type="password"
-            />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-text-field
-              id="id_password2"
-              v-model="password2"
-              label="Confirm password"
-              type="password"
-            />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-btn
-              id="id_submit"
-              @click="changePassword"
-            >
-              Change password
-            </v-btn>
-          </v-col>
-        </v-row>
+        <v-form
+          ref="form"
+          lazy-validation
+        >
+          <v-row>
+            <v-col>Change password</v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                id="id_password1"
+                v-model="password1"
+                :label="$t('lblPassword')"
+                type="password"
+                :rules="[
+                  requiredRule($t('lblPassword')),
+                  matchingPasswordsRule(password2)
+                ]"
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                id="id_password2"
+                v-model="password2"
+                :label="$t('lblConfirmPassword')"
+                type="password"
+                :rules="[
+                  requiredRule($t('lblConfirmPassword')),
+                  matchingPasswordsRule(password1)
+                ]"
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-btn
+                id="id_submit"
+                @click="changePassword"
+              >
+                Change password
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
       </v-container>
     </template>
   </app-view>
@@ -47,11 +60,14 @@ import AppView from '../components/AppView.vue';
 import {ref} from 'vue';
 import {useStore} from 'vuex';
 import {useRoute} from 'vue-router';
+import {matchingPasswordsRule, requiredRule} from '../helpers/rules.js';
 
 export default {
   name: 'ChangePassword',
   components: {AppView},
   setup() {
+    const form = ref(null);
+
     const password1 = ref('');
     const password2 = ref('');
 
@@ -63,15 +79,27 @@ export default {
      * Sends a change password request with the entered password to the backend
      */
     function changePassword() {
-      axios.post('/registration/password/reset/confirm/',
-          {
-            uid: route.params.uid,
-            token: route.params.token,
-            new_password1: password1.value,
-            new_password2: password2.value,
-          });
+      form.value.validate().then((result) => {
+        if (result.valid) {
+          axios.post('/registration/password/reset/confirm/',
+              {
+                uid: route.params.uid,
+                token: route.params.token,
+                new_password1: password1.value,
+                new_password2: password2.value,
+              });
+        }
+      });
     }
-    return {password1, password2, changePassword};
+
+    return {
+      form,
+      password1,
+      password2,
+      changePassword,
+      matchingPasswordsRule,
+      requiredRule,
+    };
   },
 };
 </script>
