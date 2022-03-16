033843ede41cf44570ef3885ae9c78e0b31b4f52
Lukas <l.loos95@gmail.com>
2022-03-16 22:32:49 +0100

    docs(adapters): add documentation

--- python_anywhere/accounts/adapters.py
+++ python_anywhere/accounts/adapters.py
@@ -1,10 +1,21 @@
+"""This file contains custom django allauth adapters"""
+
 from allauth.account.adapter import DefaultAccountAdapter
 
 from python_anywhere.accounts.models import User
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
+    """
+    Adapter for overriding inactive users instead of trying to create new ones
+    """
     def save_user(self, request, user, form, commit=True):
+        """
+        When called with commit=False (which is the case in
+        dj_rest_auth/registration/serializers.py:239), checks if inactive user
+        with same email-address as provided user exists and assigns its id to
+        make sure user is updated instead of inserted.
+        """
         user = super().save_user(request, user, form, commit)
         if not commit:
             inactive_user_id = User.all_objects\
