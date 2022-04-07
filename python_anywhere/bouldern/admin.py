14a8e66ae6df4ab4da4b198bdfbfce2eebe9edfd
Lukas <l.loos95@gmail.com>
2022-04-07 18:02:42 +0200

    chore(models): configure model history

--- python_anywhere/bouldern/admin.py
+++ python_anywhere/bouldern/admin.py
@@ -1,12 +1,13 @@
 """This file is the home of model representations in the admin interface"""
 from django.contrib.admin import site
+from simple_history.admin import SimpleHistoryAdmin
 
 from python_anywhere.bouldern.models import Boulder, Gym, Color, Grade, \
     Ascent, FavoriteGym
 
-site.register(Color)
-site.register(Boulder)
-site.register(Grade)
-site.register(Gym)
-site.register(Ascent)
-site.register(FavoriteGym)
+site.register(Color, SimpleHistoryAdmin)
+site.register(Boulder, SimpleHistoryAdmin)
+site.register(Grade, SimpleHistoryAdmin)
+site.register(Gym, SimpleHistoryAdmin)
+site.register(Ascent, SimpleHistoryAdmin)
+site.register(FavoriteGym, SimpleHistoryAdmin)
